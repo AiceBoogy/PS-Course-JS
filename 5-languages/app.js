c9b2415db410b8cfa344cc6c014fbae82a36a4ed
Никита Оганесян <aiceboogy@yandex.ru>
2024-01-17 13:18:45 +0300

    homework-2

---
+++ 5-languages/app.js
@@ -0,0 +1,13 @@
+let userLang = navigator.language;
+
+switch (userLang) {
+  case "ru":
+    console.log("Привет!");
+    break;
+  case "en":
+    console.log("Hello!");
+    break;
+  case "de":
+    console.log("Gutten tag!");
+    break;
+}
