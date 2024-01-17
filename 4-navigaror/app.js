9f11b1a29df630edcc0e13c3ffc0f9e5fc2a8308
Никита Оганесян <aiceboogy@yandex.ru>
2024-01-17 13:18:30 +0300

    Homework-1

---
+++ 4-navigaror/app.js
@@ -0,0 +1,9 @@
+let addressLat = 10;
+let addressLong = 10;
+let positionLat = 2;
+let positionLong = 2;
+console.log(
+  `Чтобы попасть в назначенное место нужно продвинуться на ${
+    addressLat - positionLat
+  } шагов по оси x и ${addressLong - positionLong} шагов по оси y`
+);
